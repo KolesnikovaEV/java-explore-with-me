7de8ed8e31ba17102d82575ba412da52596d8c20
«KolesnikovaEV» <homyachokk96@yandex.ru>
2023-09-29 17:12:35 +0300

    Дипломный проект. 2 этап

---
+++ stats-service/stats-client/src/main/java/ru/practicum/statsclient/client/StatsClientConfig.java
@@ -0,0 +1,14 @@
+package ru.practicum.statsclient.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class StatsClientConfig {
+
+    @Bean
+    public RestTemplate rest() {
+        return new RestTemplate();
+    }
+}
