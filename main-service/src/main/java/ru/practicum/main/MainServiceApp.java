8878fecf223b5a2edec0d7bb75eb0ca288f68eca
«KolesnikovaEV» <homyachokk96@yandex.ru>
2023-09-29 20:59:11 +0300

    Дипломный проект. 2 этап

--- main-service/src/main/java/ru/practicum/main/MainServiceApp.java
+++ main-service/src/main/java/ru/practicum/main/MainServiceApp.java
@@ -3,9 +3,9 @@ package ru.practicum.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum"})
 public class MainServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApp.class, args);
+        SpringApplication.run(ru.practicum.main.MainServiceApp.class, args);
     }
 }
